eabf8e0b85b067f835373ef7be896471053f8bab
Daniel Rodriguez <daniel@danielrodriguez.info>
2023-10-13 23:40:41 -0400

    Using TDD - Added skeleton of a Kiteboarding Calculator, types and very simple unit tests

--- Tests/KiteboardingKitTests/KiteboardingKitTests.swift
+++ Tests/KiteboardingKitTests/KiteboardingKitTests.swift
@@ -2,11 +2,36 @@ import XCTest
 @testable import KiteboardingKit
 
 final class KiteboardingKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    var sut: KiteboardingCalculatorType!
+    
+    override func setUp() {
+        super.setUp()
+        sut = KiteboardingCalculator()
+    }
+    
+    func testTrainerKite() throws {
+        let result = sut.calculateTrainer()
+        XCTAssertEqual(result.speed.ideal, nil)
+        XCTAssertEqual(result.speed.maximum, 10)
+        XCTAssertEqual(result.speed.minimum, 24)
+        
+        XCTAssertEqual(result.board, .init(length: 72, width: 19.1, area: 1213.5))
+    }
+    
+    func testKite() throws {
+        let result = sut.calculate(weight: 200)
+        XCTAssertEqual(result.speed.ideal, 20)
+        XCTAssertEqual(result.speed.maximum, 30)
+        XCTAssertEqual(result.speed.minimum, 15)
+        
+        XCTAssertEqual(result.kiteSize.ideal, 9.9)
+        XCTAssertEqual(result.kiteSize.maximum, 14.8)
+        XCTAssertEqual(result.kiteSize.minimum, 7.4)
+        
+        XCTAssertEqual(result.boardOptions.beginner, .init(length: 72, width: 19.1, area: 1213.5))
+        XCTAssertEqual(result.boardOptions.lightWind, .init(length: 63.6, width: 19.1, area: 1212.1))
+        XCTAssertEqual(result.boardOptions.normalWind, .init(length: 59.3, width: 17.5, area: 934.9))
+        XCTAssertEqual(result.boardOptions.hardWind, .init(length: 54.2, width: 16.1, area: 786.1))
     }
 }
